fa8920bda4a5851d7d720d773ef2477927f4caaa
kavitaui <122557323+kavitaui@users.noreply.github.com>
2024-08-30 10:59:41 +0100

    start my project

---
+++ src/components/NoProjectSelected.js
@@ -0,0 +1,10 @@
+export default function NoProjectSelected() {
+    return (
+        <> <img></img>
+        <h2>No Project Selected</h2>
+        <p>Select a project or get start with a new one</p>
+            <button>create new project</button>
+        </>
+       
+    )
+}
