b8ff1cbce542bbfde781bc359cdfa167e4db1d59
kavitaui <122557323+kavitaui@users.noreply.github.com>
2024-09-04 19:16:30 +0100

    on add save project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 
 import './App.css';
+import { useState } from 'react';
 import ProjectsSideBar from './components/ProjectsSideBar';
 import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects:[]
+
+  })
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId:null
+      }
+    })
+  }
+  let content;
+  if (projectsState.selectedProjectId === null) {
+   content= <NewProject/>
+  }
+  else if (projectsState.selectedProjectId === undefined) {
+    content =<NoProjectSelected  onStartAddProject ={handleStartAddProject}/>
+  }
   return (
-    <main className="h-screen my-8 flex-gap-8">
+    <main className="h-screen my-8 flex gap-8">
     
   
-      <ProjectsSideBar /> 
+      <ProjectsSideBar onStartAddProject ={handleStartAddProject} /> 
 
-      <NewProject />  
-      <NoProjectSelected/>
+     {content}
    
     
      
--- src/components/NoProjectSelected.js
+++ src/components/NoProjectSelected.js
@@ -1,10 +1,14 @@
-export default function NoProjectSelected() {
+import noProjectsImage from "../Assets/no-projects.png";
+import Button from "./Button";
+export default function NoProjectSelected({onStartAddProject}) {
+
     return (
-        <> <img></img>
-        <h2>No Project Selected</h2>
-        <p>Select a project or get start with a new one</p>
-            <button>create new project</button>
-        </>
+        <div className=" mt-16 text-center w-2/3">
+            <img src={noProjectsImage} alt="an empty task list"  className ="w-16 h-16 object-contain mx-auto"/>
+        <h2 className ="text-stone-500 text-xl font-bold my-4">No Project Selected</h2>
+        <p classname= "text-stone-400 mb-4">Select a project or get start with a new one</p>
+          <p className="mt-8"><Button onClick={onStartAddProject}>create new project</Button></p>  
+        </div>
        
     )
 }
